48e4272265a8409db5c5356f6935a455a0c21f43
Florian Haas <florian@hastexo.com>
2013-07-07 16:19:41 +0000

    Rename db to database

--- manifests/node/controller.pp
+++ manifests/node/controller.pp
@@ -1,8 +1,9 @@
-class kickstack::cloudcontroller inherits kickstack {
+class kickstack::node::controller inherits kickstack {
 
   include kickstack::rpc
-  include kickstack::db
+  include kickstack::database
 
   kickstack::db::service { ['keystone','glance','cinder','nova']: }
 
+  include kickstack::glance::registry
 }
