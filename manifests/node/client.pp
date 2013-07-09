ce4979473a0c3de6a1514d688105823df3a6ba97
Florian Haas <florian@hastexo.com>
2013-07-09 10:39:06 +0000

    Temporarily disable cinder client

--- manifests/node/client.pp
+++ manifests/node/client.pp
@@ -1,6 +1,6 @@
 class kickstack::node::client inherits kickstack {
   include kickstack::keystone::client
   include kickstack::glance::client
-  include kickstack::cinder::client
+#  include kickstack::cinder::client
   include kickstack::quantum::client
 }
