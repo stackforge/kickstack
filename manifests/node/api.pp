196f4bde4e01d4585cac6cdceabe3c68a2799a17
Florian Haas <florian@hastexo.com>
2013-07-14 07:39:59 +0000

    Move endpoint definitions to auth node

--- manifests/node/api.pp
+++ manifests/node/api.pp
@@ -1,14 +1,10 @@
 class kickstack::node::api inherits kickstack {
   include kickstack::glance::api
-  include kickstack::glance::endpoint
 
   include kickstack::cinder::api
-  include kickstack::cinder::endpoint
 
   include kickstack::quantum::server
-  include kickstack::quantum::endpoint
   include kickstack::quantum::plugin
 
   include kickstack::nova::api
-  include kickstack::nova::endpoint
 }
--- manifests/node/auth.pp
+++ manifests/node/auth.pp
@@ -1,4 +1,8 @@
 class kickstack::node::auth inherits kickstack {
   include kickstack::keystone::api
   include kickstack::keystone::endpoint
+  include kickstack::glance::endpoint
+  include kickstack::cinder::endpoint
+  include kickstack::quantum::endpoint
+  include kickstack::nova::endpoint
 }
