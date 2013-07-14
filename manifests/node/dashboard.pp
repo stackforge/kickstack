c758845fc8311ccd7be066bbb1394b5530a18449
Florian Haas <florian@hastexo.com>
2013-07-14 12:03:49 +0000

    Fix dependencies for dashboard node

--- manifests/node/dashboard.pp
+++ manifests/node/dashboard.pp
@@ -1,4 +1,23 @@
 class kickstack::node::dashboard inherits kickstack {
-  include kickstack::horizon
-  include kickstack::nova::vncproxy
+
+  $keystone_internal_address = getvar("${fact_prefix}keystone_internal_address")
+  $nova_keystone_password = getvar("${fact_prefix}nova_keystone_password")
+
+  case $::kickstack::rpc {
+    'rabbitmq': {
+      $amqp_host = getvar("${::kickstack::fact_prefix}rabbit_host")
+      $amqp_password = getvar("${::kickstack::fact_prefix}rabbit_password")
+    }
+    'qpid': {
+      $amqp_host = getvar("${::kickstack::fact_prefix}qpid_host")
+      $amqp_password = getvar("${::kickstack::fact_prefix}qpid_password")
+    }
+  }
+
+  if $keystone_internal_address {
+    include kickstack::horizon
+    if $nova_keystone_password and $amqp_host and $amqp_password {
+      include kickstack::nova::vncproxy
+    }
+  }
 }
