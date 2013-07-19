851af1885c36787c14c64bc95f9e9e3cb93dfd31
Florian Haas <florian@hastexo.com>
2013-07-19 12:22:40 +0000

    Conditionally add Quantum client configuration to compute node

--- manifests/node/compute.pp
+++ manifests/node/compute.pp
@@ -17,12 +17,16 @@ class kickstack::node::compute inherits kickstack {
 
   $nova_sql_conn = getvar("${::kickstack::fact_prefix}nova_sql_connection")
   $nova_keystone_password = getvar("${::kickstack::fact_prefix}nova_keystone_password")
+  $quantum_keystone_password = getvar("${::kickstack::fact_prefix}quantum_keystone_password")
   $glance_api_host = getvar("${::kickstack::fact_prefix}glance_api_host")
 
   if $amqp_host and $amqp_password {
     include kickstack::quantum::agent::l2
     if $nova_sql_conn and $nova_keystone_password and $glance_api_host {
       include kickstack::nova::compute
+    }
+    if $quantum_keystone_password {
+      include kickstack::nova::quantumclient
     }
   }
 }
