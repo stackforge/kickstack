02fe5f5fac916e54c00338cce224fbf5bd8d27db
Florian Haas <florian@hastexo.com>
2013-07-27 07:57:35 +0200

    puppet-lint fixes

--- manifests/endpoint.pp
+++ manifests/endpoint.pp
@@ -7,19 +7,19 @@ define kickstack::endpoint ( $service_password ) {
   $classname = "${servicename}::keystone::auth"
 
   # Installs the service user endpoint.
-  class { "${classname}":
-    password         => $service_password,
-    public_address   => "${hostname}${::kickstack::keystone_public_suffix}",
-    admin_address    => "${hostname}${::kickstack::keystone_admin_suffix}",
+  class { $classname:
+    password => $service_password,
+    public_address => "${hostname}${::kickstack::keystone_public_suffix}",
+    admin_address => "${hostname}${::kickstack::keystone_admin_suffix}",
     internal_address => $hostname,
-    region           => "$::kickstack::keystone_region",
-    require          => Class['::keystone'],
+    region => $::kickstack::keystone_region,
+    require => Class['::keystone'],
   }
 
-  kickstack::exportfact::export { "${factname}":
-    value => "${service_password}",
-    tag => "${servicename}",
-    require => Class["${classname}"]
+  kickstack::exportfact::export { $factname:
+    value => $service_password,
+    tag => $servicename,
+    require => Class[$classname]
   }
 
 }
