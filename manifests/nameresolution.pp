88cd1cbe6fb0399461ed9f78d35f32160b85d29a
Florian Haas <florian@hastexo.com>
2013-10-17 15:32:20 +0200

    Add FQDN to host name resolution, if available

--- manifests/nameresolution.pp
+++ manifests/nameresolution.pp
@@ -9,8 +9,15 @@ class kickstack::nameresolution inherits kickstack {
 
   case $::kickstack::name_resolution {
     'hosts': {
-      @@host { $hostname:
+      $host = pick($fqdn,$hostname)
+      if $fqdn {
+        $aliases = [ $hostname ]
+      } else {
+        $aliases = []
+      }
+      @@host { $host:
         ip => getvar("ipaddress_${::kickstack::nic_management}"),
+        host_aliases => $aliases,
         comment => 'Managed by Puppet',
         tag => 'hostname'
       }
