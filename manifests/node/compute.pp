5d5bf55ac43bfadf6a73bbd890632834d3f214f9
Florian Haas <florian@hastexo.com>
2013-10-19 10:31:31 +0200

    Add Ceilometer compute agent to compute node

--- manifests/node/compute.pp
+++ manifests/node/compute.pp
@@ -19,6 +19,9 @@ class kickstack::node::compute inherits kickstack {
   $nova_keystone_password = getvar("${::kickstack::fact_prefix}nova_keystone_password")
   $neutron_keystone_password = getvar("${::kickstack::fact_prefix}neutron_keystone_password")
   $glance_api_host = getvar("${::kickstack::fact_prefix}glance_api_host")
+  $ceilometer_sql_conn = getvar("${::kickstack::fact_prefix}ceilometer_sql_connection")
+  $ceilometer_keystone_password = getvar("${::kickstack::fact_prefix}ceilometer_keystone_password")
+  $ceilometer_metering_secret = getvar("${::kickstack::fact_prefix}ceilometer_metering_secret")
 
   if $amqp_host and $amqp_password {
     include kickstack::neutron::agent::l2::compute
@@ -27,6 +30,9 @@ class kickstack::node::compute inherits kickstack {
     }
     if $neutron_keystone_password {
       include kickstack::nova::neutronclient
+    }
+    if $ceilometer_sql_conn and $ceilometer_keystone_password and $ceilometer_metering_secret {
+      include kickstack::ceilometer::agent::compute
     }
   }
 }
